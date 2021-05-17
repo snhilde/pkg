88e0e47ab5954b62ea1b8b58348e04b563f28f4e
Hilde N <snhilde@protonmail.com>
2021-05-16 22:58:56 -0400

    add function New and global var InvalidPkg

--- pkg.go
+++ pkg.go
@@ -8,8 +8,42 @@ import (
 	"go/parser"
 )
 
+var (
+	InvalidPkg = fmt.Errorf("invalid package")
+)
+
 type Package struct {
 	buildPackage *build.Package
 	astPackage   *ast.Package
 	docPackage   *doc.Package
+}
+
+func New(importPath string) (*Package, error) {
+	buildPackage, err := build.Import(importPath, "", 0)
+	if err != nil {
+		return nil, err
+	}
+
+	fset := token.NewFileSet()
+	pkgs, err := parser.ParseDir(fset, p.Dir, nil, parser.AllErrors)
+	if err != nil {
+		return nil, err
+	}
+
+	astPackage, ok := pkgs[buildPackage.Name]
+	if !ok {
+		return nil, fmt.Errorf("package not found in %s", importPath)
+	}
+
+	docPackage := doc.New(astPackage, importPath, 0)
+	if doc == nil {
+		return nil, InvalidPkg
+	}
+
+	p := new(Package)
+	p.buildPackage = buildPackage
+	p.astPackage   = astPackage
+	p.docPackage   = docPackage
+
+	return p, nil
 }
