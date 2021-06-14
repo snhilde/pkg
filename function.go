2997dc3ef6e17855a5ccfbaeec388dbf26d163fb
Hilde N <snhilde@protonmail.com>
2021-06-14 08:34:47 -0400

    import go/doc

--- function.go
+++ function.go
@@ -1,8 +1,10 @@
 package pkg
 
 import (
+	"go/doc"
 )
 
 // Function holds information about an exported function in a package.
 type Function struct {
+	docFunc *doc.Func
 }
--- type.go
+++ type.go
@@ -1,10 +1,12 @@
 package pkg
 
 import (
+	"go/doc"
 )
 
 // Type holds information about an exported type in a package.
 type Type struct {
+	docType *doc.Type
 }
 
 // Method holds information about a type's method.
