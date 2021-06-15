6784d9259356a9952c20118d9cc1083a8aca2000
Hilde N <snhilde@protonmail.com>
2021-06-15 19:49:04 -0400

    add Type's Functions, Methods, and Exports

--- type.go
+++ type.go
@@ -6,7 +6,17 @@ import (
 
 // Type holds information about an exported type in a package.
 type Type struct {
+	// Type object from go/doc.
 	docType *doc.Type
+}
+
+func (t Type) Functions() []Function {
+}
+
+func (t Type) Methods() []Method {
+}
+
+func (t Type) Exports() []interface{} {
 }
 
 // Method holds information about a type's method.
