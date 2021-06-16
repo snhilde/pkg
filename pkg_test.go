407e185ebd0e1d2694f608ce3f5e60b5bf66d311
Hilde N <snhilde@protonmail.com>
2021-06-15 20:33:25 -0400

    add TestFiles

--- pkg_test.go
+++ pkg_test.go
@@ -53,9 +53,85 @@ func TestName(t *testing.T) {
 		want := members[len(members)-1]
 		have := p.Name()
 		if want != have {
-			t.Error("incorrect package name")
+			t.Errorf("%s: incorrect package name", testPackage)
 			t.Log("\twant:", want)
 			t.Log("\thave:", have)
+		}
+	}
+}
+
+// TestFiles checks that pkg correctly reports the correct source files in each test package.
+func TestFiles(t *testing.T) {
+	// These are the files in each package. We're going to hard-code these values so that we can
+	// achieve repeatable accuracy.
+	fileMap := map[string][]string{
+		"errors": {
+			"errors.go",
+			"wrap.go",
+		},
+		"fmt": {
+			"doc.go",
+			"errors.go",
+			"format.go",
+			"print.go",
+			"scan.go",
+		},
+		"hash": {
+			"hash.go",
+		},
+		"archive/tar": {
+			"common.go",
+			"format.go",
+			"reader.go",
+			"stat_actime1.go",
+			"stat_actime2.go",
+			"stat_unix.go",
+			"strconv.go",
+			"writer.go",
+		},
+		"unicode": {
+			"casetables.go",
+			"digit.go",
+			"graphic.go",
+			"letter.go",
+			"tables.go",
+		},
+		"net/rpc": {
+			"client.go",
+			"debug.go",
+			"server.go",
+		},
+	}
+
+	for testPackage, files := range fileMap {
+		p, _ := pkg.New(testPackage)
+
+		want := files
+		have := p.Files()
+
+		// First, let's make sure that we have the correct number of files.
+		if len(want) != len(have) {
+			t.Errorf("%s: incorrect file list", testPackage)
+			t.Log("\twant:", want)
+			t.Log("\thave:", have)
+		}
+
+		// Then, let's check that each file is present in the returned list.
+		for _, w := range want {
+			found := false
+			for _, h := range have {
+				if w == h {
+					// If we've already found this file, then something is wrong.
+					if found {
+						t.Errorf("%s: duplicate file in list: %s", testPackage, w)
+						return
+					}
+					found = true
+				}
+			}
+			if !found {
+				t.Errorf("%s: missing file: %s", testPackage, w)
+			}
 		}
 	}
 }
