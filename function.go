ec732a84804df27d67b211afcfeb25455c42381b
Hilde N <snhilde@protonmail.com>
2021-06-17 08:00:55 -0400

    Function: add isValid sanity checker, add Name

--- function.go
+++ function.go
@@ -8,3 +8,26 @@ import (
 type Function struct {
 	docFunc *doc.Func
 }
+
+// isValid checks whether or not f is a valid Function object.
+func (f Function) isValid() bool {
+	if f == (Function{}) {
+		return false
+	}
+
+	if f.docFunc == nil {
+		return false
+	}
+
+	// All checks passed.
+	return true
+}
+
+// Name returns the function's name.
+func (f Function) Name() string {
+	if !f.isValid() {
+		return ""
+	}
+
+	return f.docFunc.Name
+}
