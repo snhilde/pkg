dcdd9213ad3ae0eb1d1aa254e4435a1870f8fb40
Hilde N <snhilde@protonmail.com>
2021-06-17 08:01:13 -0400

    Type: add isValid sanity checker, add Name

--- type.go
+++ type.go
@@ -10,13 +10,39 @@ type Type struct {
 	docType *doc.Type
 }
 
+// isValid checks whether or not t is a valid Type object.
+func (t Type) isValid() bool {
+	if t == (Type{}) {
+		return false
+	}
+
+	if t.docType == nil {
+		return false
+	}
+
+	// All checks passed.
+	return true
+}
+
+// Name returns the type's name.
+func (t Type) Name() string {
+	if !t.isValid() {
+		return ""
+	}
+
+	return t.docType.Name
+}
+
 func (t Type) Functions() []Function {
+	return nil
 }
 
 func (t Type) Methods() []Method {
+	return nil
 }
 
 func (t Type) Exports() []interface{} {
+	return nil
 }
 
 // Method holds information about a type's method.
