a5fd35af6cfc045db70b53ed019fb846ec26aaf7
Hilde N <snhilde@protonmail.com>
2021-06-19 19:40:20 -0400

    Method: mirror Function (except for pointer member

--- method.go
+++ method.go
@@ -1,22 +1,66 @@
+// This file contains the information and logic for the Method type.
 package pkg
 
 import (
+	"bytes"
 	"go/doc"
+	"strings"
 )
 
 // Method holds information about a type's method.
 type Method struct {
 	// Func object from go/doc.
-	docMethod *doc.Func
+	docFunc *doc.Func
+
+	// Method's name.
+	name string
+
+	// Original declaration in source for this method.
+	decl *bytes.Reader
+
+	// Input parameters.
+	input []Parameter
+
+	// Output parameters.
+	output []Parameter
+
+	// Whether or not the receiver of this method is a pointer.
+	pointer bool
 }
 
-// isValid checks whether or not m is a valid Method object.
-func (m Method) isValid() bool {
-	if m == (Method{}) {
+// newMethod builds a new Method object based on go/doc's Func.
+func newMethod(f *doc.Func, r *bytes.Reader) Method {
+	if f == nil || r == nil {
+		return Method{}
+	}
+	// Read out the source declaration.
+	start, end := f.Decl.Type.Pos()-1, f.Decl.Type.End()-1 // -1 to index properly
+	decl := extractSource(r, start, end)
+
+	// Extract the parameters.
+	in, out := extractParameters(f.Decl.Type, r)
+
+	return Method{
+		docFunc: f,
+		name:    f.Name,
+		decl:    decl,
+		input:   in,
+		output:  out,
+		pointer: strings.HasPrefix(f.Recv, "*"),
+	}
+}
+
+// IsValid checks whether or not m is a valid Method object.
+func (m Method) IsValid() bool {
+	if m.docFunc == nil {
 		return false
 	}
 
-	if m.docMethod == nil {
+	if m.name == "" {
+		return false
+	}
+
+	if m.decl.Len() == 0 {
 		return false
 	}
 
@@ -26,9 +70,30 @@ func (m Method) isValid() bool {
 
 // Name returns the method's name.
 func (m Method) Name() string {
-	if !m.isValid() {
+	if !m.IsValid() {
 		return ""
 	}
 
-	return m.docMethod.Name
+	return m.name
+}
+
+// Input returns a list of input parameters sent to this method, or nil on invalid object. If
+// there are no input parameters, this returns a slice of size 0.. The list does not include the
+// method receiver.
+func (m Method) Input() []Parameter {
+	if !m.IsValid() {
+		return nil
+	}
+
+	return m.input
+}
+
+// Output returns a list of output parameters returned from this method, or nil on invalid object.
+// If there are no output parameters, this returns a slice of size 0..
+func (m Method) Output() []Parameter {
+	if !m.IsValid() {
+		return nil
+	}
+
+	return m.output
 }
