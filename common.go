64751801481d8c1e4a9dde149466dad66d35dd8c
Hilde N <snhilde@protonmail.com>
2021-06-19 15:21:43 -0400

    getSource -> extractSource, take io.ReaderAt and token.Pos args to extractSource

--- common.go
+++ common.go
@@ -3,25 +3,20 @@ package pkg
 
 import (
 	"bytes"
+	"go/token"
 	"io"
 )
 
-// getSource gets the source text in r between the start and end positions.
-func getSource(r *bytes.Reader, start, end int) []byte {
-	if start < 0 || start >= end || end >= r.Len() {
+// extractSource extracts the source text in r between the start and end positions.
+func extractSource(r io.ReaderAt, start, end token.Pos) *bytes.Reader {
+	if start < 0 || start >= end {
 		return nil
 	}
 
-	// Move to the beginning of this segment of code.
-	if n, err := r.Seek(int64(start), io.SeekStart); n != int64(start) || err != nil {
-		return nil
-	}
-
-	// Read in the source.
 	b := make([]byte, end - start)
-	if n, err := r.Read(b); n != end - start || err != nil {
+	if n, err := r.ReadAt(b, int64(start)); n != int(end - start) || err != nil {
 		return nil
 	}
 
-	return b
+	return bytes.NewReader(b)
 }
